503ba3fe36ab3851e41ef5d1f931b717e00cce82
Enmanuel Urbina <enmanuelu100@gmail.com>
2024-04-04 00:19:04 -0600

    Esto es solo una prueba

--- Guia.sql
+++ Guia.sql
@@ -17,3 +17,14 @@ CREATE TABLE Produtos
     descricao VARCHAR(100) NOT NULL,
     preco DECIMAL(10,2) NOT NULL
 );
+
+CREATE TABLE Pedidos
+(
+    id INT PRIMARY KEY IDENTITY,
+    id_cliente INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL,
+    data_pedido DATE NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id),
+    FOREIGN KEY (id_produto) REFERENCES Produtos(id)
+);
